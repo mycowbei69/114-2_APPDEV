bfd30b6d718f1015a80dc6155f775c58493185ee
mycowbei69 <C113181144@nkust.edu.tw>
2026-03-26 20:34:48 +0800

    Refactor MarineLife class and its subclasses

--- week05/OceanSimulator.java
+++ week05/OceanSimulator.java
@@ -1,15 +1,12 @@
-class MarineLife{
+class MarineLife {
     protected String name;
     protected String habitat;
 
     public MarineLife(String name, String habitat) {
         this.name = name;
         this.habitat = habitat;
-    } 
-    public String kingdom() {
-        return name + " 動物界";
     }
-    
+
     public String move() {
         return name + " 在水中移動";
     }
@@ -17,32 +14,23 @@ class MarineLife{
     public String eat() {
         return name + " 正在覓食";
     }
-    
-    public String feed() {
-        return name + " 正在覓食";
-    }
 
-    public String feed(String food) {
-        return name + " 正在吃" +food;
-    }
-
-    public String feed(String food, int amount) {
-        return name + " 吃了 " + amount + " 份 " + food;
-    }
-    
     public String info() {
         return name + "（" + habitat + "）";
     }
 }
 
-class Shark extends MarineLife {
+// TODO 1: Shark 繼承 MarineLife
+// - 覆寫 move() → "[name] 高速衝刺獵食"
+// - 覆寫 eat() → "[name] 撕咬獵物"
+class Shark extends MarineLife{
     public Shark(String name, String habitat) {
         super(name, habitat);
     }
 
     @Override
     public String move() {
-        return name + " 高速衝刺";
+        return name + " 高速衝刺獵食";
     }
 
     @Override
@@ -50,21 +38,12 @@ class Shark extends MarineLife {
         return name + " 撕咬獵物";
     }
 
-    public String feed() {
-        return name + " 正在覓食";
-    }
-
-    public String feed(String food) {
-        return name + " 正在吃" +food;
-    }
-
-    public String feed(String food, int amount) {
-        return name + " 吃了 " + amount + " 份 " + food;
-    }
 }
 
-
-class Turtle extends MarineLife {
+// TODO 2: Turtle 繼承 MarineLife
+// - 覆寫 move() → "[name] 緩慢划動四肢"
+// - 覆寫 eat() → "[name] 啃食海草"
+class Turtle extends MarineLife{
     public Turtle(String name, String habitat) {
         super(name, habitat);
     }
@@ -78,63 +57,44 @@ class Turtle extends MarineLife {
     public String eat() {
         return name + " 啃食海草";
     }
+
 }
 
-class Dolphin extends MarineLife {
+// TODO 3: Dolphin 繼承 MarineLife
+// - 覆寫 move() → "[name] 躍出水面再潛入"
+// - 覆寫 eat() → "[name] 合作圍捕魚群"
+class Dolphin extends MarineLife{
     public Dolphin(String name, String habitat) {
         super(name, habitat);
     }
 
     @Override
     public String move() {
-        return name + " 躍出水面";
+        return name + " 躍出水面再潛入";
     }
 
     @Override
     public String eat() {
         return name + " 合作圍捕魚群";
     }
+
 }
 
-class Octopus extends MarineLife {
-    public Octopus(String name, String habitat) {
-        super(name, habitat);
-    }
+public class OceanSimulator {
+    public static void main(String[] args) {
+        System.out.println("=== 海洋模擬器 ===\n");
 
-    @Override
-    public String move() {
-        return name + " 噴射水流推進";
-    }
-
-    @Override
-    public String eat() {
-        return name + " 用觸手捕捉獵物";
-    }
-}
-public class MarineEcosystem {
-    
-
-     public static void main(String[] args) {
-        final int OCEAN_DEPTH=11034;
-        System.out.println("海洋最深處：" + OCEAN_DEPTH + " 公尺\n");
-
-        
+        // TODO 4: 建立 MarineLife 陣列，放入三個物件
         MarineLife[] ocean = {
-           
             new Shark("大白鯊", "深海"),
             new Turtle("綠蠵龜", "珊瑚礁"),
-            new Dolphin("瓶鼻海豚", "近海") 
+            new Dolphin("瓶鼻海豚", "近海")
         };
 
-        for (MarineLife c : ocean) {
-            System.out.println(c.info());
-            System.out.println("  分類：" + c.kingdom());
-            System.out.println("  移動：" + c.move());
-            System.out.println("  覓食：" + c.eat());
-            System.out.println("  覓食：" + c.eat());
-            System.out.println("  餵食：" + c.feed());
-            System.out.println("  餵食：" + c.feed("小魚"));
-            System.out.println("  餵食：" + c.feed("小魚", 3));
+        for (MarineLife creature : ocean) {
+            System.out.println(creature.info());
+            System.out.println("  移動：" + creature.move());
+            System.out.println("  覓食：" + creature.eat());
             System.out.println();
         }
     }
